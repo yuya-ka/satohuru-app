3af872c60e9ca4e5a316d9dd2ae50ca8c31b1381
yuya-ka <yuya.kato@divx.co.jp>
2022-12-19 15:36:43 +0900

    axiosの導入

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,23 @@
 import styled from "styled-components";
-
 import Header from '../components/Header.jsx';
 import Main from '../components/Main.jsx';
 import Footer from '../components/Footer.jsx';
 
-/* ---------------- css ---------------- */
 const StyledHome = styled.div`
   height: 100%;
   width: 100%;
   margin: 0;
 `;
 
-
-
-/* ---------------- jsx ---------------- */
 const Home = () => {
   return (
     <StyledHome>
-
       <Header />
-      
       <Main />
-
       <Footer />
-
     </StyledHome>
   );
 }
-
 
 //別のファイルでインポート(読み込み)出来るようにする
 export default Home;
